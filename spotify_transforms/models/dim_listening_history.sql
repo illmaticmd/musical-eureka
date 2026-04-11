d019e2929d7bae9b278fc12dda8b498046c7324c
McDesmond Duru <mcdesmond.duru@icloud.com>
2026-04-11 17:47:31 -0400

    changed names to resolve conflict

--- spotify_transforms/models/dim_listening_history.sql
+++ spotify_transforms/models/dim_listening_history.sql
@@ -1,4 +1,4 @@
-WITH tracks AS (
+WITH track_data AS (
     SELECT DISTINCT
         track.id AS track_id,
         track.name AS track_name,
@@ -8,26 +8,7 @@ WITH tracks AS (
     FROM `spotify-data-pipeline-490402.spotify_raw.listening_history`
 ),
 
-artists AS (
-    SELECT DISTINCT
-        a.artist_id,
-        a.artist_name,
-        COALESCE(
-            NULLIF(a.genres[SAFE_OFFSET(0)], ''),
-            'Unknown'
-        ) AS primary_genre
-    FROM `spotify-data-pipeline-490402.spotify_raw.artists` a
-)WITH tracks AS (
-    SELECT DISTINCT
-        track.id AS track_id,
-        track.name AS track_name,
-        track.popularity AS popularity,
-        played_at,
-        track.artists[SAFE_OFFSET(0)].id AS primary_artist_id
-    FROM `spotify-data-pipeline-490402.spotify_raw.listening_history`
-),
-
-artists AS (
+artist_data AS (
     SELECT DISTINCT
         a.artist_id,
         a.artist_name,
@@ -48,17 +29,6 @@ SELECT
     a.artist_name,
     a.primary_genre,
     t.popularity
-FROM tracks t
-LEFT JOIN artists a
-    ON t.primary_artist_id = a.artist_id
-
-SELECT
-    t.played_at,
-    t.track_id,
-    t.track_name,
-    a.artist_name,
-    a.primary_genre,
-    t.popularity
-FROM tracks t
-LEFT JOIN artists a
+FROM track_data t
+LEFT JOIN artist_data a
     ON t.primary_artist_id = a.artist_id
