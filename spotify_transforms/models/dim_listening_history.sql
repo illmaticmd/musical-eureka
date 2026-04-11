3cd8ee63f0d69658f1223f1707e8d75dfa0f4114
McDesmond Duru <mcdesmond.duru@icloud.com>
2026-04-11 17:29:00 -0400

    updated model to include enrichment join

--- spotify_transforms/models/dim_listening_history.sql
+++ spotify_transforms/models/dim_listening_history.sql
@@ -17,7 +17,40 @@ artists AS (
             'Unknown'
         ) AS primary_genre
     FROM `spotify-data-pipeline-490402.spotify_raw.artists` a
+)WITH tracks AS (
+    SELECT DISTINCT
+        track.id AS track_id,
+        track.name AS track_name,
+        track.popularity AS popularity,
+        played_at,
+        track.artists[SAFE_OFFSET(0)].id AS primary_artist_id
+    FROM `spotify-data-pipeline-490402.spotify_raw.listening_history`
+),
+
+artists AS (
+    SELECT DISTINCT
+        a.artist_id,
+        a.artist_name,
+        COALESCE(
+            NULLIF(a.genres[SAFE_OFFSET(0)], ''),
+            e.inferred_genre,
+            'Unknown'
+        ) AS primary_genre
+    FROM `spotify-data-pipeline-490402.spotify_raw.artists` a
+    LEFT JOIN `spotify-data-pipeline-490402.spotify_raw.artist_genre_enrichment` e
+        ON a.artist_id = e.artist_id
 )
+
+SELECT
+    t.played_at,
+    t.track_id,
+    t.track_name,
+    a.artist_name,
+    a.primary_genre,
+    t.popularity
+FROM tracks t
+LEFT JOIN artists a
+    ON t.primary_artist_id = a.artist_id
 
 SELECT
     t.played_at,
